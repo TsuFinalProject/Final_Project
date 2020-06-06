feb7b64c9bc6b366b613d82604624d7f733dad39
Tamar Nasrashvili <tamari.nasrashvili0@outlook.com>
2020-06-06 14:52:15 +0400

    first upload

---
+++ OrganisationArchive.DAL/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace OrganisationArchive.DAL
+{
+    public class Class1
+    {
+    }
+}
---
+++ OrganisationArchive.DAL/OrganizationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganisationArchive.DAL
+{
+    public class OrganizationDbContext : DbContext
+    {
+        public OrganizationDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
