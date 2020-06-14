e7167b47182838686321944207b19bfa13cb3c1c
Mariami-137 <Mariami-137@github.com>
2020-06-14 22:03:02 +0400

    add employee repository

---
+++ OrganisationArchive.DAL/Repository/Interfaces/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganisationArchive.DAL.Repository
+{
+    public interface IRepositoryBase<T>
+    {
+       
+            IEnumerable<T> FindAll();
+
+            T Get(int id);
+
+            void Create(T entity);
+
+            void Update(T entity);
+
+            void Delete(T entity);
+    }
+}
