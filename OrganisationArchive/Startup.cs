b92fd6f42ec286512854156c92c7b34239d1fa77
Tamar Nasrashvili <tamari.nasrashvili0@outlook.com>
2020-06-18 21:21:32 +0400

    add organization views

--- OrganisationArchive/Startup.cs
+++ OrganisationArchive/Startup.cs
@@ -71,7 +71,7 @@ namespace OrganisationArchive
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Person}/{action=People}/{id?}");
             });
         }
     }
