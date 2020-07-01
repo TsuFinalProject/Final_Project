2475c39afcbfe3bf9936855f5f84f7f08c098799
nat-hub <nata.shengelia1@gmail.com>
2020-07-01 16:57:41 +0400

    add enum position

---
+++ OrganisationArchive.DAL/Enums/Position.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganisationArchive.DAL.Enums
+{
+    public enum Position
+    {   Accountant,
+        Administrator,
+        Analyst,
+        CEO,
+        Clerk, 
+        Coordinator,
+        Developer,
+        Director,
+        Engineer,
+        Manager,
+        Secretary
+
+
+
+    }
+}
